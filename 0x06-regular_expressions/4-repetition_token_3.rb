86cb616f2f2c79daeb46ad55f2bea321c5c5cab4
samuelalfred3 <samuelalfrednjuguna@gmail.com>
2024-02-02 13:48:35 +0300

    Fixed Errors

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,12 +1,3 @@
 #!/usr/bin/env ruby
-
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <input_string>"
-  exit 1
-end
-
-input_string = ARGV[0]
-regex = /hbt{2,5}n/
-matches = input_string.scan(regex)
-puts matches.join
+puts ARGV[0].scan(/hbt*n/).join
 
